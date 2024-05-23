5af53744b9575bf0b4fee0faab73f25a49a9354f
Lorenzo Mantero <lorenzomantero2002@gmail.com>
2024-05-23 15:38:12 +0200

    start

---
+++ main.py
@@ -0,0 +1,12 @@
+# external dependencies
+import pandas as pd
+import os
+
+
+# pandas
+#path_game_data_csv = 'https://github.com/Cinofix/analisi-e-rappresentazione-dati/raw/main/data/dipendenti.csv'
+#data = pd.read_csv(path_game_data_csv)
+
+#data.info()
+
+print(os.getcwd())
