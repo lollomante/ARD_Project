d182fc7ef91e577b1839ea07af33b8beaa1622c9
Lorenzo Mantero <lorenzomantero2002@gmail.com>
2024-06-18 16:01:06 +0200

    top graphs

---
+++ Dashboard/main.py
@@ -0,0 +1,16 @@
+# external dependencies
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import dash_bootstrap_components as dbc
+from dash import Dash, Input, Output, html, dcc
+import plotly.express as px
+
+# internal dependencies
+from graph import *
+
+
+porcodio()
+
+
